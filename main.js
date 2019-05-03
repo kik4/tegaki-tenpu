ec0e7ccbdcaacdfa12403c73866cde9660f1409c
kik4 <kik4.contact@gmail.com>
2019-05-03 17:57:24 +0900

    background color

--- main.js
+++ main.js
@@ -45,6 +45,8 @@ const canvasDraw = imgsrc => {
     cv.width = width;
     cv.height = height;
     const ctx = cv.getContext("2d");
+    ctx.fillStyle = "rgb(240, 224, 214)";
+    ctx.fillRect(0, 0, maxWidth, maxHeight);
     ctx.drawImage(image, 0, 0, width, height);
   };
   image.src = imgsrc;
