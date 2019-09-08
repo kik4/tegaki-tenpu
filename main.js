8866c753c3f54abd61ed9bfc996a5281c9a59139
kik4 <kik4.contact@gmail.com>
2019-09-08 23:37:49 +0900

    update to bookmarklet

--- main.js
+++ main.js
@@ -1,56 +1,70 @@
-const maxWidth = 400;
-const maxHeight = 400;
+javascript: (() => {
+  const maxWidth = 400;
+  const maxHeight = 400;
 
-const input = document.createElement("input");
-
-const onchange = e => {
-  if (input.files && input.files.length == 0) {
-    alert("ファイルを選択してください");
-    return;
-  }
-  const file = input.files[0];
-  if (!file.type.match("image.*")) {
-    alert("画像を選択してください");
-    return;
-  }
-  const reader = new FileReader();
-  reader.onload = () => {
-    imgsrc = reader.result;
-    canvasDraw(imgsrc);
-  };
-  reader.readAsDataURL(file);
-};
-
-const canvasDraw = imgsrc => {
-  const image = new Image();
-  image.onload = () => {
-    const cv = document.getElementById("oejs");
-    if (!cv) {
-      alert("手書き機能をオンにしてください");
+  const onchange = e => {
+    if (input.files && input.files.length == 0) {
+      alert("ファイルを選択してください");
       return;
     }
-    let width, height;
-    if (image.width > maxWidth || image.height > maxHeight) {
-      if (image.width > image.height) {
-        width = maxWidth;
-        height = (maxWidth * image.height) / image.width;
-      } else {
-        height = maxHeight;
-        width = (maxHeight * image.width) / image.height;
-      }
-    } else {
-      width = image.width;
-      height = image.height;
+    const file = input.files[0];
+    if (!file.type.match("image.*")) {
+      alert("画像を選択してください");
+      return;
     }
-    cv.width = width;
-    cv.height = height;
-    const ctx = cv.getContext("2d");
-    ctx.clearRect(0, 0, maxWidth, maxHeight);
-    ctx.drawImage(image, 0, 0, width, height);
+    const reader = new FileReader();
+    reader.onload = () => {
+      imgsrc = reader.result;
+      canvasDraw(imgsrc);
+    };
+    reader.readAsDataURL(file);
   };
-  image.src = imgsrc;
-};
 
-input.setAttribute("type", "file");
-input.addEventListener("change", onchange);
-document.body.append(input);
+  const canvasDraw = imgsrc => {
+    const image = new Image();
+    image.onload = () => {
+      let width, height;
+      if (image.width > maxWidth || image.height > maxHeight) {
+        if (image.width > image.height) {
+          width = maxWidth;
+          height = (maxWidth * image.height) / image.width;
+        } else {
+          height = maxHeight;
+          width = (maxHeight * image.width) / image.height;
+        }
+      } else {
+        width = image.width;
+        height = image.height;
+      }
+
+      const canvas = document.createElement("canvas");
+      canvas.id = "oejs";
+      canvas.width = width;
+      canvas.height = height;
+      const ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, maxWidth, maxHeight);
+      ctx.drawImage(image, 0, 0, width, height);
+
+      const oldCanvas = document.querySelector("#oejs");
+      if (oldCanvas) oldCanvas.remove();
+      document.querySelectorAll("#ftbl")[0].appendChild(canvas);
+
+      /* access grobally */
+      tegakiJs.oeUpdate = () => {
+        var dataUri = canvas.toDataURL("image/png");
+        dataUri = dataUri.replace(/^.*,/, "");
+        var b = document.getElementById("baseform");
+        if (b) {
+          b.value = dataUri;
+        }
+        return true;
+      };
+    };
+    image.src = imgsrc;
+  };
+
+  const input = document.createElement("input");
+  input.type = "file";
+  input.addEventListener("change", onchange);
+  input.click();
+})();
